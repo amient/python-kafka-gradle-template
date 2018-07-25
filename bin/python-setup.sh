154bbce945c6c06d5246cc7693d4f9f3a2fcdc53
michal-harish <michal.harish@gmail.com>
2018-07-25 16:34:36 +0100

    more efficient python-setup.sh script

--- bin/python-setup.sh
+++ bin/python-setup.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
-DIR="$( cd $(dirname ${BASH_SOURCE[0]}) && pwd )"
+################################################################################################################
 
+reuquired=( "avro-python3:1.8.2" \
+            "avro-gen:0.3.0" \
+            "confluent-kafka:0.11.0" \
+            "confluent-kafka:0.11.5")
+
+################################################################################################################
+
+
+
+DIR="$( cd $(dirname ${BASH_SOURCE[0]}) && pwd )"
 LOCAL_REPO_DIR="$HOME/pypi-ivy"
 PIVY_IMPORTER_VERSION="0.7.4"
 
@@ -27,14 +37,19 @@ fi
 #clean broken cached downloads
 find $LOCAL_REPO_DIR -name *.tar.gz | while read file; do tar -tzf $file  > /dev/null; if [ $? -ne 0 ]; then echo "fixing: $file"; rm $file; fi done
 
+
+do_update=""
+for package in "${reuquired[@]}"
+do
+    if [ ! -d "$LOCAL_REPO_DIR/pypi/${package//://}" ]; then
+        do_update="$do_update $package"
+    fi
+done
+
+if [ ! -z "$do_update" ]; then
 java -jar $DIR/build/pygradle/build/pivy-importer/libs/pivy-importer-0.7.4-SNAPSHOT-all.jar \
     --replace Babel:0.8=Babel:1.0 \
     --replace python-dateutil:1.0=python-dateutil:2.4.1 \
     --repo "$LOCAL_REPO_DIR" \
-    avro-python3:1.8.2 \
-    avro-gen:0.3.0 \
-    frozendict:1.2 \
-    tzlocal:1.5.1 \
-    datadog:0.21.0 \
-    confluent-kafka:0.11.0 \
-    confluent-kafka:0.11.5
+    $do_update
+fi
